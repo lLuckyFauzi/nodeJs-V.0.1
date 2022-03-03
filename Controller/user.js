994dc775eaeb5fc3308fd4a390225aebf898bf88
lLuckyFauzi <luckyfauzi2004@gmail.com>
2022-03-03 21:21:47 +0700

    Deploy

--- Controller/user.js
+++ Controller/user.js
@@ -105,11 +105,10 @@ module.exports = {
         },
         include: [{ model: Notes }],
       });
-
       if (!data) {
         res.status(404).json({ message: "Not Found!" });
       }
-      res.json({ data });
+      res.status(200).json({ data });
     } catch (error) {
       res.status(422).json({ message: error.messsage });
     }
