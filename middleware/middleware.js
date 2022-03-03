ac749b9369a10c10005e5b40e70175eb81722938
lLuckyFauzi <luckyfauzi2004@gmail.com>
2022-03-03 21:43:00 +0700

    Deploy

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -3,12 +3,14 @@ const jwt = require("jsonwebtoken");
 const middleware = (req, res, next) => {
   const token = req.headers.authorization;
   if (!token) {
-    res.status(404).json({ message: "Cannot find user, Please login first" });
+    return res
+      .status(404)
+      .json({ message: "Cannot find user, Please login first" });
   }
 
   const user = jwt.decode(token, process.env.TOKEN);
   if (!user) {
-    res.status(401).json({ message: `User not found!` });
+    return res.status(401).json({ message: `User not found!` });
   }
   req.payload = user;
   next();
